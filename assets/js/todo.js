837c2d8e4f7e370235be4a7d113be3bb10a1c10b
Asmaa H Abbas <abbas.asmaa@gmail.com>
2017-09-22 16:06:06 -0400

    added app functionality

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,9 +2,25 @@
 
 //select an individual todo
 //manipulate it (with a line through)
-$("li").on("click", (event) => {
-	let currLi = $(event.target);
+$("ul").on("click", "li", (event) => {
+	let currLi = $(event.currentTarget);
 	currLi.toggleClass("completed");
 });
 
 //click on X to delete todo
+$("ul").on("click", ".delete", (event) => {
+	let that = $(event.currentTarget);
+	that.parent().fadeOut(500, function () {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+//add listener to text input that fires when hit the enter key
+$("input[type='text']").on("keypress", (event) => {
+	let currInput = $(event.currentTarget);
+	if(event.which === 13){
+		$("ul").append(`<li><span class="delete">X </span>${currInput.val()}</li>`);
+		currInput.val("");
+	}
+});
